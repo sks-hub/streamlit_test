0352b4baa26540564c2452adb2db59f8a1a6494d
sks-hub <sunilksh@outlook.com>
2021-06-08 12:06:31 -0700

    modifying the port

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"michaelc@thedataincubator.com\"\n\
+email = \"xyz@thedataincubator.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
-port = $PORT\n\
+port = 5000\n\
 " > ~/.streamlit/config.toml
